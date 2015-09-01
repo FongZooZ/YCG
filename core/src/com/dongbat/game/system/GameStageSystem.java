22603458f9f867588a7e0d6711d6165e7529a770
zbill <myheaven_1993@yahoo.com.vn>
2015-09-01 18:14:10 +0700

    add cooldown

--- core/src/com/dongbat/game/system/GameStageSystem.java
+++ core/src/com/dongbat/game/system/GameStageSystem.java
@@ -8,19 +8,25 @@ package com.dongbat.game.system;
 import com.artemis.BaseSystem;
 import com.artemis.Entity;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.dongbat.game.component.UnitMovement;
+import com.dongbat.game.stage.AbilityButton;
+import com.dongbat.game.stage.RadialSprite;
 import com.dongbat.game.util.AbilityUtil;
+import com.dongbat.game.util.ECSUtil;
 import com.dongbat.game.util.EntityUtil;
 import com.dongbat.game.util.InputUtil;
 import com.dongbat.game.util.UuidUtil;
@@ -37,6 +43,10 @@ public class GameStageSystem extends BaseSystem {
   private Skin skin = new Skin(Gdx.files.internal("skins/uiskin.json"), new TextureAtlas(Gdx.files.internal("skins/uiskin.atlas")));
   private Label title = new Label("Full game play is coming soon, with multiplayer feature over WIFI and internet", skin);
   private Touchpad touchpad;
+  private AbilityButton buttonFleeAbility = new AbilityButton("Flee", skin, "circle.png");
+  private AbilityButton buttonBlowAbility = new AbilityButton("HotBlow", skin, "circle.png");
+  private AbilityButton buttonSplitAbility = new AbilityButton("SplitAndJoin", skin, "circle.png");
+  private AbilityButton buttonVacuumAbility = new AbilityButton("Vacuum", skin, "circle.png");
 
   public GameStageSystem() {
   }
@@ -56,93 +66,43 @@ public class GameStageSystem extends BaseSystem {
     float width = worldWidth / 60;
     float height = worldHeight / 60;
 
-//    TextButton flee = new TextButton("Flee", skin);
-    ImageButton flee = new ImageButton(skin);
-    flee.setSize(width * 7, width * 7);
-    flee.setPosition(worldWidth - flee.getWidth(), height * 2);
-    flee.addListener(new ClickListener() {
-
-      @Override
-      public void clicked(InputEvent event, float x, float y) {
-        UUID localPlayerId = LocalPlayerUtil.getLocalPlayer(world);
-        Entity e = UuidUtil.getEntityByUuid(world, localPlayerId);
-        if (e == null || localPlayerId == null) {
-          return;
-        }
-        UnitMovement move = EntityUtil.getComponent(world, e, UnitMovement.class);
-        Vector2 destination = move.getDirectionVelocity();
-        AbilityUtil.use(world, e, "Flee", destination);
-      }
-
-    });
-
-//    TextButton hotBlow = new TextButton("Blow", skin);
-    ImageButton hotBlow = new ImageButton(skin);
-    hotBlow.setSize(width * 7, width * 7);
-    hotBlow.setPosition(worldWidth - flee.getWidth(), height * 2 + width * 7);
-    hotBlow.addListener(new ClickListener() {
-
-      @Override
-      public void clicked(InputEvent event, float x, float y) {
-        UUID localPlayerId = LocalPlayerUtil.getLocalPlayer(world);
-        Entity e = UuidUtil.getEntityByUuid(world, localPlayerId);
-        if (e == null || localPlayerId == null) {
-          return;
-        }
-        UnitMovement move = EntityUtil.getComponent(world, e, UnitMovement.class);
-        Vector2 destination = move.getDirectionVelocity();
-        AbilityUtil.use(world, e, "HotBlow", destination);
-      }
-    });
-
-//    TextButton spitAndJoin = new TextButton("Split", skin);
-    ImageButton spitAndJoin = new ImageButton(skin);
-    spitAndJoin.setSize(width * 7, width * 7);
-    spitAndJoin.setPosition(worldWidth - flee.getWidth(), height * 2 + width * 14);
-    spitAndJoin.addListener(new ClickListener() {
-
-      @Override
-      public void clicked(InputEvent event, float x, float y) {
-        UUID localPlayerId = LocalPlayerUtil.getLocalPlayer(world);
-        Entity e = UuidUtil.getEntityByUuid(world, localPlayerId);
-        if (e == null || localPlayerId == null) {
-          return;
-        }
-        UnitMovement move = EntityUtil.getComponent(world, e, UnitMovement.class);
-        Vector2 destination = move.getDirectionVelocity();
-        AbilityUtil.use(world, e, "SplitAndJoin", destination);
-      }
-    });
-
-//    TextButton vacuum = new TextButton("Vacuum", skin);
-    ImageButton vacuum = new ImageButton(skin);
-    vacuum.setSize(width * 7, width * 7);
-    vacuum.setPosition(worldWidth - flee.getWidth(), height * 2 + width * 21);
-    vacuum.addListener(new ClickListener() {
-
-      @Override
-      public void clicked(InputEvent event, float x, float y) {
-        UUID localPlayerId = LocalPlayerUtil.getLocalPlayer(world);
-        Entity e = UuidUtil.getEntityByUuid(world, localPlayerId);
-        if (e == null || localPlayerId == null) {
-          return;
-        }
-        UnitMovement move = EntityUtil.getComponent(world, e, UnitMovement.class);
-        Vector2 destination = move.getDirectionVelocity();
-        AbilityUtil.use(world, e, "Vacuum", destination);
-      }
-    });
-
+    buttonFleeAbility.createNormalButton(width * 7, width * 7, worldWidth - width * 7, height * 2);
+    buttonFleeAbility.createRadialSprite(width * 7, width * 7, 1000, height * 2);
+    buttonBlowAbility.createNormalButton(width * 7, width * 7, worldWidth - width * 7, height * 2 + width * 7);
+    buttonBlowAbility.createRadialSprite(width * 7, width * 7, 1000, height * 2 + width * 7);
+    buttonSplitAbility.createNormalButton(width * 7, width * 7, worldWidth - width * 7, height * 2 + width * 14);
+    buttonSplitAbility.createRadialSprite(width * 7, width * 7, 1000, height * 2 + width * 14);
+    buttonVacuumAbility.createNormalButton(width * 7, width * 7, worldWidth - width * 7, height * 2 + width * 21);
+    buttonVacuumAbility.createRadialSprite(width * 7, width * 7, 1000, height * 2 + width * 21);
+    //    ImageButton flee = createNormalButton(width * 7, width * 7, worldWidth - width * 7, height * 2, "Flee", stage, skin);
+    //
+//        ImageButton hotBlow = createNormalButton(width * 7, width * 7, worldWidth - flee.getWidth(), height * 2 + width * 7, "HotBlow", stage, skin);
+    //    ImageButton spitAndJoin = createNormalButton(width * 7, width * 7, worldWidth - flee.getWidth(), height * 2 + width * 14, "SplitAndJoin", stage, skin);
+    //    ImageButton vacuum = createNormalButton(width * 7, width * 7, worldWidth - flee.getWidth(), height * 2 + width * 21, "Vacuum", stage, skin);
+    //    TextureRegion textureRegion = new TextureRegion(new Texture(Gdx.files.internal("circle.png")));
+    //    radialSprite = new RadialSprite(textureRegion);
+    //
+    //    image = new Image(radialSprite);
+    //    image.setSize(width * 7, width * 7);
+    //    image.setPosition(1000, height * 2);
+    //    stage.addActor(image);
+    //    image.setVisible(true);
+    //    float angle = AbilityUtil.getLastCast(world, null, null);
+    //    radialSprite.setAngle(angle);
     title.setPosition(worldWidth - width * 30, worldHeight - height * 2);
 
     touchpad = new Touchpad(worldWidth / 40, skin);
     touchpad.setBounds(worldWidth / 50, worldWidth / 50, worldWidth / 6, worldWidth / 6);
 
     stage.addActor(touchpad);
-    stage.addActor(flee);
-    stage.addActor(hotBlow);
-    stage.addActor(spitAndJoin);
-    stage.addActor(vacuum);
+    stage.addActor(buttonFleeAbility.imageButton);
+    stage.addActor(buttonFleeAbility.radialImage);
+    stage.addActor(buttonBlowAbility.imageButton);
+    stage.addActor(buttonBlowAbility.radialImage);
+    stage.addActor(buttonSplitAbility.imageButton);
+    stage.addActor(buttonSplitAbility.radialImage);
+    stage.addActor(buttonVacuumAbility.imageButton);
+    stage.addActor(buttonVacuumAbility.radialImage);
     stage.addActor(title);
     InputUtil.addProcessor(stage, 0);
   }
@@ -150,11 +110,16 @@ public class GameStageSystem extends BaseSystem {
   @Override
   protected void processSystem() {
     stage.act();
+    buttonFleeAbility.radialSprite.setAngle(ECSUtil.getFrame(world) % 360 + 20);
+    buttonBlowAbility.radialSprite.setAngle(ECSUtil.getFrame(world) % 360 + 120);
+    buttonSplitAbility.radialSprite.setAngle(ECSUtil.getFrame(world) % 360 + 240);
+    buttonVacuumAbility.radialSprite.setAngle(ECSUtil.getFrame(world) % 360 + 300);
+
     if (touchpad.isTouched()) {
       float x = touchpad.getKnobPercentX();
       float y = touchpad.getKnobPercentY();
 
-      UUID localPlayerId = LocalPlayerUtil.getLocalPlayer(world);
+      UUID localPlayerId = LocalPlayerUtil.getLocalPlayer();
       Entity e = UuidUtil.getEntityByUuid(world, localPlayerId);
       if (e == null || localPlayerId == null) {
         return;
